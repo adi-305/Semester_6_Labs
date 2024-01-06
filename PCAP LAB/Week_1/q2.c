55e61b12c4cfccdba905834d95e9f6e8c780ac7f
Aditya Singhvi <102035044+adi-305@users.noreply.github.com>
2024-01-06 10:33:00 +0530

    Update q2.c

--- PCAP LAB/Week_1/q2.c
+++ PCAP LAB/Week_1/q2.c
@@ -2,11 +2,9 @@
 #include <stdio.h>
 
 int main(int argc,char *argv[]){
-	int rank,size;
-
+	int rank;
 	MPI_Init(&argc,&argv);
 	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
-	MPI_Comm_size(MPI_COMM_WORLD,&size);
 	if(rank % 2 == 0) printf("Rank = %d  Hello\n",rank);
 	else printf("Rank = %d  World\n",rank);
 	MPI_Finalize();
