03977e114348095f0fb7b09e9ff607c92875691b
Aditya Singhvi <102035044+adi-305@users.noreply.github.com>
2024-01-06 10:33:20 +0530

    Update q3.c

--- PCAP LAB/Week_1/q3.c
+++ PCAP LAB/Week_1/q3.c
@@ -2,12 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(int argc,char *argv[]){
-	int rank,size;
+	int rank;
 	int a = atoi(argv[1]);
 	int b = atoi(argv[2]);
 	MPI_Init(&argc,&argv);
 	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
-	MPI_Comm_size(MPI_COMM_WORLD,&size);
 	switch(rank){
 		case 0:
 			printf("%d + %d = %d\n",a,b,a+b);
