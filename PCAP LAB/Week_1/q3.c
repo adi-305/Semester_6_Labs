be28dba8ab615999b48bb2710eccb204a2897df7
Aditya Singhvi <102035044+adi-305@users.noreply.github.com>
2024-01-06 09:57:20 +0530

    Add files via upload

---
+++ PCAP LAB/Week_1/q3.c
@@ -0,0 +1,29 @@
+#include "mpi.h"
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc,char *argv[]){
+	int rank,size;
+	int a = atoi(argv[1]);
+	int b = atoi(argv[2]);
+	MPI_Init(&argc,&argv);
+	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+	MPI_Comm_size(MPI_COMM_WORLD,&size);
+	switch(rank){
+		case 0:
+			printf("%d + %d = %d\n",a,b,a+b);
+			break;
+		case 1:
+			printf("%d - %d = %d\n",a,b,a-b);
+			break;
+		case 2:
+			printf("%d / %d = %d\n",a,b,a/b);
+			break;
+		case 3:
+			printf("%d * %d = %d\n",a,b,a*b);
+			break;
+		default:
+			printf("Rank is out of range\n");
+	}
+	MPI_Finalize();
+	return 0;
+}
