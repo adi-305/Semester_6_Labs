93ca8830655789a5b7b37561487beca9ea97b798
Aditya Singhvi <102035044+adi-305@users.noreply.github.com>
2024-01-06 10:34:01 +0530

    Update q4.c

--- PCAP LAB/Week_1/q4.c
+++ PCAP LAB/Week_1/q4.c
@@ -1,24 +1,20 @@
 #include "mpi.h"
 #include <stdio.h>
 #include <string.h> 
-
-
 char* toggle(int n,char *s){
 	char c = s[n];
 	int a = c;
 	char* ans;
 	strcpy(ans,s);
-	if(a > 96) ans[n] = c-32;
-	else ans[n] = c+32;
+	if(a > 96) ans[n]-=32;
+	else ans[n]+=32;
 	return ans;
 }
-
 int main(int argc,char *argv[]){
-	int rank,size;
+	int rank;
 	int len = strlen(argv[1]);
 	MPI_Init(&argc,&argv);
 	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
-	MPI_Comm_size(MPI_COMM_WORLD,&size);
 	if(rank > len){
 		printf("Process rank(%d) is greater than the length of the string\n",rank);
 	}
