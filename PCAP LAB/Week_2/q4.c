d739be29cb4cddfea20a7e1106cee78accab09fc
Aditya Singhvi <102035044+adi-305@users.noreply.github.com>
2024-01-13 10:59:32 +0530

    Update q4.c

--- PCAP LAB/Week_2/q4.c
+++ PCAP LAB/Week_2/q4.c
@@ -1,30 +1,41 @@
 #include "mpi.h"
 #include <stdio.h>
-#include <string.h> 
+#include <stdlib.h>
+#include <string.h>
 
-
-char* toggle(int n,char *s){
-	char c = s[n];
-	int a = c;
-	char* ans;
-	strcpy(ans,s);
-	if(a > 96) ans[n] = c-32;
-	else ans[n] = c+32;
-	return ans;
-}
 
 int main(int argc,char *argv[]){
 	int rank,size;
-	int len = strlen(argv[1]);
+	int number;
 	MPI_Init(&argc,&argv);
 	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
 	MPI_Comm_size(MPI_COMM_WORLD,&size);
-	if(rank > len){
-		printf("Process rank(%d) is greater than the length of the string\n",rank);
+	MPI_Status status;
+	if(rank == 0){
+		printf("Enter the Number that you want to send : ");
+		scanf("%d",&number);
+		MPI_Send(&number,1,MPI_INT,1,1,MPI_COMM_WORLD);
+		MPI_Recv(&number,1,MPI_INT,size-1,1,MPI_COMM_WORLD,&status);
+		printf("In process %d recieved from process %d. Number : %d\n",0,size-1,number);
+		fflush(stdout);
 	}
 	else{
-		char *ans  = toggle(rank,argv[1]);
-		printf("%s\n",ans);
+		for(int i =1 ;i<size;i++){
+			if(i == size-1){
+				MPI_Recv(&number,1,MPI_INT,i-1,1,MPI_COMM_WORLD,&status);
+				printf("In process %d recieved from process %d. Number : %d\n",i,i-1,number);
+				number += 1;
+				MPI_Send(&number,1,MPI_INT,0,1,MPI_COMM_WORLD);
+				fflush(stdout);
+			}
+			if(i == rank){
+				MPI_Recv(&number,1,MPI_INT,i-1,1,MPI_COMM_WORLD,&status);
+				printf("In process %d recieved from process %d. Number : %d\n",i,i-1,number);
+				number += 1;
+				MPI_Send(&number,1,MPI_INT,i+1,1,MPI_COMM_WORLD);
+				fflush(stdout);
+			}
+		}
 	}
 	MPI_Finalize();
 	return 0;
