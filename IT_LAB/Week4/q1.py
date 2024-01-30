d0a8d2ed7b6b23f83facbbb6cd8d1203b287f6f4
Aditya Singhvi <102035044+adi-305@users.noreply.github.com>
2024-01-30 10:28:07 +0530

    Update q1.py

--- IT_LAB/Week4/q1.py
+++ IT_LAB/Week4/q1.py
@@ -1,14 +1,17 @@
-def subset(i , arr,string):
-	if( i >= len(arr)):
-		print(string)
-		return
 
-	subset(i+1, arr, string +" " + str(arr[i]))
-	subset(i+1,arr,string)
+class solution:
+	def subset(self,i , arr,string):
+		if( i >= len(arr)):
+			print(string)
+			return
+	
+		subset(self,i+1, arr, string +" " + str(arr[i]))
+		subset(self,i+1,arr,string)
 
 
 
 
+y = solution()
 arr = [4,5,6]
 string = ""
 subset(0,arr,string)
